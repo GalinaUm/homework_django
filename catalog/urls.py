0ce35e04a61698ccc51ac579218a33cf4301e730
marseilleumirzak <galinaumerzakhova@yandex.ru>
2026-01-04 16:21:37 +0300

    base

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, index
+from catalog.views import home, contacts, products_list
 
 app_name = CatalogConfig.name
 
@@ -8,6 +8,6 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('home/', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('', index,)
+    path('', products_list,)
 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
     return render(request, 'home.html')
@@ -23,7 +25,9 @@ def contacts(request):
     return render(request, 'contacts.html')
 
 
-def index(request):
-    return render(request, 'base.html')
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'base.html', context)
 
 
