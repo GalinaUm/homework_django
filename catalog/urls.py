3d6265405e6f3c2e0fe54332e8e6585f1879a12a
marseilleumirzak <galinaumerzakhova@yandex.ru>
2026-01-04 19:04:21 +0300

    product_details

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, products_list
+from catalog.views import home, contacts, products_list, product_details
 
 app_name = CatalogConfig.name
 
@@ -8,6 +8,6 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('home/', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('', products_list, name='products_list')
-
+    path('', products_list, name='products_list'),
+    path('catalog/<int:pk>', product_details, name='product_details'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
 
@@ -29,5 +29,11 @@ def products_list(request):
     products = Product.objects.all()
     context = {"products": products}
     return render(request, 'products_list.html', context)
+
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'product_details.html', context)
+
 
 
